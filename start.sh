beed79cb736630e706bb9ea876c071968a0c9e4c
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-02-03 09:04:55 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/PiroAutoFilterBot_v2 /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
